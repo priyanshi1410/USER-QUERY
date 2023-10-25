12126130202b2fb7450cf2cde9653594130607f9
priyanshiitaliya1409 <priyanshiitaliya32@gmail.com>
2023-10-25 10:12:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express=require('express')
+const connect=require("./db")
+const user = require('./schema')
+const app=express()
+app.use(express.json())
+
+
+app.get("/", async (req, res) => {
+    const data = await user.find({ gender: "Female" })
+    res.send(data)
+})
+app.get("/"),async(req,res)=>{
+    const data=await user.find({gender: "Female", language: {$in: ["Kannada", "Hindi"]}})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({$or: [{gender: "Male", language: "Hindi"}, {gender: "Female", language: "Kannada"}]})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({shirt_size: "S"})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({gender: "Female", shirt_size: "XL"})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({$or: [{gender: "Male", language: "English"}, {gender: "Female", language: "Hindi"}]})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({$or: [{gender: "Male", language:{$in:["English","Hindi"]}}, {gender: "Female", language:{$in:["Kannada","German"]}}]})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({$or: [{gender: "Female", language:"Bengali",shirt_size:"XL"},{gender: "Male", language:"German",shirt_size:{$in:['L','M']}}]})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({gender: "Female", language:{$in:["Hindi","Punjabi", "Bengali", "Gujarati", "Tamil", "Malayalam"]}})
+    res.send(data)
+}
+app.get("/"),async(req,res)=>{
+    const data=await user.find({gender: "Male", language:"Korean"})
+    res.send(data)
+}
+
+
+
+app.listen(9000,()=>{
+    console.log("listening part 8090");
+    connect()
+});
